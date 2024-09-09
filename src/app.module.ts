80bba7dddf3383c8891671e2580daab0bc27fc6e
Lucas Reis <lucasreisribeiro97@gmail.com>
2024-09-08 23:29:48 -0300

    refactor: :recycle; enabling authentication for whole app routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   imports: [UserModule, AuthModule, PrismaModule],
 })
 export class AppModule {}
