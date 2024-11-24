77e49f513ab783b9ae699a0e2f59957caf19f87b
Lucas Reis <lucasreisribeiro97@gmail.com>
2024-11-24 17:50:14 -0300

    feat: :sparkles: adding class-validator and validator transformer

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Colet - API')
+    .setDescription('Serviço de Gerenciamento de Coleta de Resíduos')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory);
 
   app.enableCors();
 
