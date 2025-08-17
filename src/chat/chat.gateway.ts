b06846d812ce3a774c3db818bd3370fdaaa36230
Bertonsin <gustavobertonsin@hotmail.com>
2025-08-17 20:43:53 -0300

    feat: added message to chat when sending a message

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,24 +1,31 @@
 import { Logger } from "@nestjs/common";
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket } from "socket.io";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+  constructor(readonly prisma: PrismaService) { }
+
   readonly logger = new Logger(ChatGateway.name);
   connections = new Map<string, Map<string, Socket>>();
-  chats = new Map<string, Map<string,Socket>>()
+  chats = new Map<string, Map<string, Socket>>()
 
   @WebSocketServer()
   server: Socket
 
   @SubscribeMessage('message')
-  handleEvent(client: Socket, data: string) {
+  async handleEvent(client: Socket, data: { chatId: string, user: string, text: string }) {
     const query = client.handshake.query;
     const chat = query.chatId as string;
-    const to = query.to as string;
-    this.sendMessage(chat, to, data)
+    const from = query.from as string;
+    this.sendMessage(chat, from, data)
+    const { messages } = await this.prisma.chat.findUnique({ where: { id: chat }, select: { messages: true } })
+    const toUser = messages?.[0].toUserId
+    await this.prisma.chat.update({ data: { messages: { create: { text: data.text, fromUserId: from, toUserId: toUser } } }, where: { id: chat } })
   }
-  
+
   sendMessage(chat: string, user: string, message: any) {
     const client = this.connections.get(chat)?.get(user);
     if (!client) {
@@ -28,7 +35,7 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
     client?.emit('message', message);
     return client.id;
   }
-  
+
   afterInit() {
     this.logger.log('Socket started');
   }
