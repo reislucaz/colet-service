952eb203b6a0724fba22a947243f52d6f8801d5f
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2025-11-01 11:48:20 -0300

    feature: adiciona configuração jwt

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,3 +1,9 @@
-export const jwtConstants = {
-  secret: 'secretKey',
-};
+import { ConfigService } from '@nestjs/config';
+import { Env } from './env-schema';
+
+export const getJwtConfig = (configService: ConfigService<Env>) => ({
+  secret: configService.get('JWT_SECRET', { infer: true }),
+  signOptions: {
+    expiresIn: configService.get('JWT_EXPIRES_IN', { infer: true }),
+  },
+});
