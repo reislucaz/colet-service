1d62d7a3047450fbdd76b2905611ef2f870891a6
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2025-11-01 11:53:48 -0300

    feature: melhora validação nos valores do stripe

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -8,7 +8,10 @@ import { PrismaService } from '../prisma/prisma.service';
 export class StripeService {
   private stripe: Stripe;
 
-  constructor(private configService: ConfigService<Env>, private prisma: PrismaService) {
+  constructor(
+    private configService: ConfigService<Env>,
+    private prisma: PrismaService,
+  ) {
     this.stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'));
   }
 
@@ -43,7 +46,11 @@ export class StripeService {
   }
 
   async getTransactions(): Promise<Stripe.BalanceTransaction[]> {
-    return (await this.stripe.balanceTransactions.list({ currency: this.configService.get('STRIPE_CURRENCY') })).data;
+    return (
+      await this.stripe.balanceTransactions.list({
+        currency: this.configService.get('STRIPE_CURRENCY'),
+      })
+    ).data;
   }
 
   async confirmPaymentIntent(
@@ -52,14 +59,17 @@ export class StripeService {
     return this.stripe.paymentIntents.retrieve(paymentIntentId);
   }
 
-  async createPaymentSession(
-    productId: string,
-  ): Promise<string> {
+  async createPaymentSession(productId: string): Promise<string> {
     const product = await this.prisma.product.findUnique({
       where: { id: productId },
     });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
     const session = await this.stripe.paymentIntents.create({
-      amount: (product.price ?? 1) * 100,
+      amount: product.price * 100,
       currency: this.configService.get('STRIPE_CURRENCY'),
       automatic_payment_methods: { enabled: true },
     });
