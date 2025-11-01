e9a1f8bf35e88136ac195a4a8fe7ce14430945bd
Gustavo Bertonsin <gustavobertonsin@hotmail.com>
2025-11-01 11:51:24 -0300

    refactor: corrige rota publica

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,15 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
-import { StripeService } from "../stripe/stripe.service";
-import { Public } from "../utils/decorators/public";
+import { Controller, Get } from '@nestjs/common';
+import { StripeService } from '../stripe/stripe.service';
 
 @Controller('wallet')
 export class WalletController {
-  constructor(private readonly stripeService: StripeService) { }
+  constructor(private readonly stripeService: StripeService) {}
 
   @Get('/')
-  @Public()
   async getWallet() {
-    const balance = (await this.stripeService.getBalance());
+    const balance = await this.stripeService.getBalance();
     return {
       available: balance.available,
       pending: balance.pending,
@@ -17,7 +15,6 @@ export class WalletController {
   }
 
   @Get('/transactions')
-  @Public()
   async getTransactions() {
     const transactions = await this.stripeService.getTransactions();
     return transactions;
